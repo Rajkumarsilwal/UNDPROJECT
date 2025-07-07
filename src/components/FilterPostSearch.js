ebea0685885a74e328b8ee90ed93fc46343e6574
Rajkumar Silwal <rajkumarsilwal2@gmail.com>
2025-07-07 14:16:48 -0400

    Deleted old code

--- src/components/FilterPostSearch.js
+++ src/components/FilterPostSearch.js
@@ -1,21 +1,3 @@
-// import { TextField } from '@mui/material';
-
-// const FilterPostSearch = ({ searchTerm, onSearch }) => {
-//   return (
-//     <TextField
-//       id="search-posts"
-//       label='Search Posts'
-//       variant='outlined'
-//       type="search"
-//       fullWidth
-//       margin='normal'
-//       value={searchTerm}
-//       onChange={(e) => onSearch(e.target.value)}
-//     />
-//   );
-// };
-
-// export default FilterPostSearch;
 import { TextField, InputAdornment, IconButton } from '@mui/material';
 import ClearIcon from '@mui/icons-material/Clear';
 
