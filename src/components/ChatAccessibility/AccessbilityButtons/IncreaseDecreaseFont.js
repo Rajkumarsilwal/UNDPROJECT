89c264a1024db46a62c4dcd5011cc8fd7ab16623
Rajkumar Silwal <rajkumarsilwal2@gmail.com>
2025-07-08 12:21:13 -0400

    classname added for react increase decrease hook

--- src/components/ChatAccessibility/AccessbilityButtons/IncreaseDecreaseFont.js
+++ src/components/ChatAccessibility/AccessbilityButtons/IncreaseDecreaseFont.js
@@ -22,15 +22,7 @@ const selectors = [
     '.president-endorsement h3',
     '.president-endorsement h2',
     '.mobile-menu-link-text',
-    '.css-2ulfj5-MuiTypography-root', // Fot postcard name
-    '.css-ol7wxs-MuiTypography-root', // For postcard Location, Likes and Reposts
-    '.css-1coh8cf-MuiTypography-root',
-    '.css-177ip5v-MuiTypography-root',
-    '.css-hav9kw-MuiTypography-root',
-    '.css-ol7wxs-MuiTypography-root',
-    '.css-ahj2mt-MuiTypography-root',
-    '.css-1vw6mcs-MuiTypography-root',
-    '.css-vtncvo-MuiTypography-root',
+    '.post-card-box-contains *',
     '.BbRtadZawZJ1QXQAlzRG h5', // Footer share your thoughts
     '.BbRtadZawZJ1QXQAlzRG h6', // Footer Links Heading
     '.J_yBuGNdyAbTehoIEu1Z a', // Footer Links
@@ -53,6 +45,7 @@ export default function useFontScaling(step = 2, min = 16, max = 36) {
             document.querySelectorAll(sel).forEach((el) => {
                 if (seen.has(el)) return;
                 seen.add(el);
+                console.log('el style values::', el);
                 const currentSize = getFontSize(el);
                 const newSize = Math.min(currentSize + step, max);
                 el.style.setProperty('font-size', `${newSize}px`, 'important');
